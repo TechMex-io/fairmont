d2918647c6078fac270fa2bae42a24c6f2c73726
Alex Rodriguez <drinkhorchata@gmail.com>
2018-03-16 16:55:50 -0600

    updates for leasing and purchasing

--- pages/Purchasing.vue
+++ pages/Purchasing.vue
@@ -11,101 +11,75 @@
         <div class="container sm:w-4/5 mx-auto">
           <div class="md:flex justify-center content-center flex-wrap">
               <p class="leading-normal font-light md:w-1/2 px-8 mb-4">
-                This wonderful 1 bedroom, 1 bathroom unit is priced to sell and perfect for the person who wants to add their own personal touch to the space.  It has a spacious combination living room and dining room area with a kitchen that opens up to the balcony. This unit is freshly painted and has new carpet in neutral tones.<br>
-              <a href="" class="button button-black mt-4 inline-block">Request an Appointment</a>
+                The Fairmont Condominium Association
+                <br>
+                1800 N. Stanton
+                <br>
+                El Paso, TX  79902
+                <br>
+                <a href="" class="button button-black mt-4 inline-block">Request an Appointment</a>
               </p>
             <div class="leading-normal font-light md:w-1/2 md:pl-24 px-8 mb-4">
-              <h3>Unit: #501</h3>
-              <h3>Bedrooms: 2</h3>
-              <h3>Bathrooms: 2 full</h3>        
-              <h3>Sq Feet: 1,600</h3>
-              <h3>Price: $90,000</h3>
+              <h3>Unit #606 - For Sale</h3>
+              <h3>1 Bedroom/1 Bath</h3>
+              <h3>878.21 square feet</h3>
+              <h3>Sales Price - $85,000</h3>
+              <h3>By appointment only.</h3>
             </div>
           </div>
         </div>
       </section>
+
+      
+      
+      
+      
+      
+      
+      
+      
 
       <section class="my-4 amenities">
         <div class="container mx-auto px-8">
 
-          <div class="sm:flex flex-wrap md:pb-8 border-b-2 border-orange-fairmont-light">
-            <div class="sm:w-1/2 sm:pr-4 mb-8">
-              <img src="http://www.chicagomag.com/images/2017/0817/C201708-312-Munoz-Oprah-Condo-kitchen.jpg" alt="" class="shadow-lg rounded" srcset="">
+          <div class="sm:flex flex-wrap md:pb-8">
+          
+            <div class="sm:w-1/2 sm:px-4 mb-8">
+              <img src="~/static/Unit 606 - Living Room and Entrance  1.jpg" alt="" class="shadow-lg rounded" srcset="">
+            
             </div>
-            <div class="sm:w-1/2 sm:pl-4 mb-8">
-              <img src="http://www.chicagomag.com/images/2017/0817/C201708-312-Munoz-Oprah-Condo-bar.jpg" alt="" class="shadow-lg rounded" srcset="">
+            <div class="sm:w-1/2 sm:px-4 mb-8">
+              <img src="~/static/Unit 606 - Living & Dining Room 2.jpg" alt="" class="shadow-lg rounded" srcset="">
+            
             </div>
-            <div class="sm:w-1/2 sm:pr-4 mb-8">
-              <img src="http://www.chicagomag.com/images/2017/0817/C201708-312-Munoz-Oprah-Condo-master-bath-tub.jpg" alt="" class="shadow-lg rounded" srcset="">
+            <div class="sm:w-1/2 sm:px-4 mb-8">
+              <img src="~/static/Unit 606 - Living Room & Dining Room 3.jpg" alt="" class="shadow-lg rounded" srcset="">
+            
             </div>
-            <div class="sm:w-1/2 sm:pl-4 mb-8">
-              <img src="http://www.chicagomag.com/images/2017/0817/C201708-312-Munoz-Oprah-Condo-living-room.jpg" alt="" class="shadow-lg rounded" srcset="">
+            <div class="sm:w-1/2 sm:px-4 mb-8">
+              <img src="~/static/Unit 606 - Kitchen 4.jpg" alt="" class="shadow-lg rounded" srcset="">
             </div>
-            <div class="sm:w-1/2 sm:pr-4 mb-8">
-              <img src="http://www.greatertorontobuilders.com/wp-content/uploads/bfi_thumb/feature-mwruojf66zwve59qadt4bq3fizovgystr821vxjhps.jpg" alt="" class="shadow-lg rounded" srcset="">
+            <div class="sm:w-1/2 sm:px-4 mb-8">
+              <img src="~/static/Unit 606 - Hallway 5.jpg" alt="" class="shadow-lg rounded" srcset="">
             </div>
-            <div class="sm:w-1/2 sm:pl-4 mb-8">
-              <img src="http://quipwhit.com/wp-content/uploads/2017/11/modern-condo-living-room-design-lovely-kitchen-room-modern-condo-kitchen-living-room-modern-new-2017-of-modern-condo-living-room-design.jpg" alt="" class="shadow-lg rounded" srcset="">
+            <div class="sm:w-1/2 sm:px-4 mb-8">
+              <div class="relative overflow-hidden shadow-lg rounded w-full h-full">
+                <img src="~/static/Unit 606 - Bathroom 6.jpg" alt="" class="absolute pin-t h-full img-center" srcset="">
+              </div>
             </div>
-            <div class="sm:w-1/2 sm:pr-4 mb-8">
-              <img src="http://quipwhit.com/wp-content/uploads/2017/11/modern-condo-living-room-design-elegant-condominium-archives-michelle-lewis-interior-design-firm-modern-of-modern-condo-living-room-design.jpg" alt="" class="shadow-lg rounded" srcset="">
+            <div class="sm:w-1/2 sm:px-4 mb-8">
+              <img src="~/static/Unit 606 - Bedroom 7.jpg" alt="" class="shadow-lg rounded" srcset="">
             </div>
-            <div class="sm:w-1/2 sm:pl-4 mb-8">
-              <img src="http://www.palmbeachmlssearch.com/wp-content/uploads/2014/02/Condo-hotel-room.jpg" alt="" class="shadow-lg rounded" srcset="">
+
+            <div class="sm:w-1/2 sm:px-4 mb-8">
+              <img src="~/static/Unit 606 - Balcony View 8.jpg" alt="" class="shadow-lg rounded" srcset="">
             </div>
+        
           </div>
         </div>
       </section>
-      <section class="purchase pt-14">
-        <div class="container sm:w-4/5 mx-auto">
-          <div class="md:flex justify-center content-center flex-wrap">
-              <p class="leading-normal font-light md:w-1/2 px-8 mb-4">
-                This wonderful 1 bedroom, 1 bathroom unit is priced to sell and perfect for the person who wants to add their own personal touch to the space.  It has a spacious combination living room and dining room area with a kitchen that opens up to the balcony. This unit is freshly painted and has new carpet in neutral tones.<br>
-              <a href="" class="button button-black mt-4 inline-block">Request an Appointment</a>
-              </p>
-            <div class="leading-normal font-light md:w-1/2 md:pl-24 px-8 mb-4">
-              <h3>Unit: #501</h3>
-              <h3>Bedrooms: 2</h3>
-              <h3>Bathrooms: 2 full</h3>        
-              <h3>Sq Feet: 1,600</h3>
-              <h3>Price: $90,000</h3>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section class="my-4 amenities">
-        <div class="container mx-auto px-8">
-
-          <div class="sm:flex flex-wrap">
-            <div class="sm:w-1/2 sm:pr-4 mb-8">
-              <img src="http://www.chicagomag.com/images/2017/0817/C201708-312-Munoz-Oprah-Condo-kitchen.jpg" alt="" class="shadow-lg rounded" srcset="">
-            </div>
-            <div class="sm:w-1/2 sm:pl-4 mb-8">
-              <img src="http://www.chicagomag.com/images/2017/0817/C201708-312-Munoz-Oprah-Condo-bar.jpg" alt="" class="shadow-lg rounded" srcset="">
-            </div>
-            <div class="sm:w-1/2 sm:pr-4 mb-8">
-              <img src="http://www.chicagomag.com/images/2017/0817/C201708-312-Munoz-Oprah-Condo-master-bath-tub.jpg" alt="" class="shadow-lg rounded" srcset="">
-            </div>
-            <div class="sm:w-1/2 sm:pl-4 mb-8">
-              <img src="http://www.chicagomag.com/images/2017/0817/C201708-312-Munoz-Oprah-Condo-living-room.jpg" alt="" class="shadow-lg rounded" srcset="">
-            </div>
-            <div class="sm:w-1/2 sm:pr-4 mb-8">
-              <img src="http://www.greatertorontobuilders.com/wp-content/uploads/bfi_thumb/feature-mwruojf66zwve59qadt4bq3fizovgystr821vxjhps.jpg" alt="" class="shadow-lg rounded" srcset="">
-            </div>
-            <div class="sm:w-1/2 sm:pl-4 mb-8">
-              <img src="http://quipwhit.com/wp-content/uploads/2017/11/modern-condo-living-room-design-lovely-kitchen-room-modern-condo-kitchen-living-room-modern-new-2017-of-modern-condo-living-room-design.jpg" alt="" class="shadow-lg rounded" srcset="">
-            </div>
-            <div class="sm:w-1/2 sm:pr-4 mb-8">
-              <img src="http://quipwhit.com/wp-content/uploads/2017/11/modern-condo-living-room-design-elegant-condominium-archives-michelle-lewis-interior-design-firm-modern-of-modern-condo-living-room-design.jpg" alt="" class="shadow-lg rounded" srcset="">
-            </div>
-            <div class="sm:w-1/2 sm:pl-4 mb-8">
-              <img src="http://www.palmbeachmlssearch.com/wp-content/uploads/2014/02/Condo-hotel-room.jpg" alt="" class="shadow-lg rounded" srcset="">
-            </div>
-          </div>
-        </div>
-      </section>
-
+      
+      
 
       <section class="relative comfort">
         <!-- <div class="absolute pin bg-black opacity-75 overlay"></div> -->
@@ -143,40 +117,10 @@ export default {
 </script>
 
 <style scoped>
-header {
-  background-image: url('~/static/fairmontbuilding2.jpg');
-  background-position-y: bottom;
-}
-.hero-text {
-  text-shadow: 1px 2px 8px rgba(0,0,0,.3);
-}
-main {
-  background: repeating-linear-gradient(
-    to right,
-    #fff,
-    #fff 60px,
-    #f7f7f7 60px,
-    #f7f7f7 61px
-);
-}
-.comfort {
-  background-image: url('~/static/epskyline.jpg');
-}
 
-.border-after:after {
-  background-color: rgb(189, 106, 5);
-  content: '';
-  display: block;
-  height: 3px;
-  width: 10%;
-}
-
-.border-after-center:after {
-  margin: 2rem auto;
-}
-
-.border-after-left:after {
-  margin: .25rem 0;
+.img-center {
+  left: 50%;
+  transform: translateX(-50%);
 }
 </style>
 
